44cb7342ca6c23d4ff14b8405cd11ce9289325af
JahoMvenge99 <arnaud.Mercier.jahomvenge@gmail.com>
2023-10-23 14:42:47 +0200

    Premier commit

---
+++ src/Forme.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+const token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNjZlM2Q4ZjhmYjk2ZmE3YmQ0NGNkYjJmODVmYjVmMCIsInN1YiI6IjY1MjAxMDA1ZWE4NGM3MDE0ZWZmNDdjOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LSNWY9VbhagOQdXNbuzk9POZ73RB5KILr1gRGf1p4cI';
+const config = {
+    headers: { Authorization: `Bearer ${token}` }
+  };
+const Form = () => {
+    const [moviesData, setMoviesData] = useState([]);
+   
+    useEffect(() => {
+        axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?
+        api_key=166e3d8f8fb96fa7bd44cdb2f85fb5f0&query=code&language=fr-FR`, config
+        )
+        .then((res) => setMoviesData(res.data.results));
+    }, []);
+    return (
+        <div className='form-component'>
+            <div className='form-container'>
+                <form>
+                    <input type='text' placeholder="entrerz le titre d'un film"
+                    id="search-input" />
+                    <input type='submit' value="Rechercher" />
+                </form>
+                <div className='btn-sort-container'>
+                    <div className='btn-srt' id="goodToBad">
+                    Top<span>→</span>
+                </div>
+                <div className='btn-sort' id="badToGood">
+                    Flop<span>→</span>
+                </div>
+            </div>
+        </div>
+        <div className='result'>RESULTAT</div>
+        {moviesData.slice(0,12).map((movie) => (
+            <h2>{movie.title}</h2>
+
+        ))}
+        </div>
+    );
+};
+
+export default Form;
---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <nav>
+                <ul>
+                        <li>
+                            <NavLink to="/Home" className={nav => nav.isActive ? "nav-active": ""}>Acceuil</NavLink>
+                            </li>
+                    
+                     <li><NavLink to="/Coups-de-coeur" className={nav => nav.isActive ? "nav-active": ""}>
+                       Coup de coeur</NavLink>
+                       </li>
+                    {/* <li>
+                    <NavLink to="/Contact" className={nav => nav.isActive ? "nav-active": ""}>
+                        Contact </NavLink>
+                        </li> */}
+                   
+                </ul>
+            </nav>
+            <h1>FIND YOUR MOUVIE</h1>
+        </div>
+    );
+};
+
+export default Header;
